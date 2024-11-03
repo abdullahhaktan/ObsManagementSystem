73bd0f5f2c60025e62cfe3acf37f03cb63d46bac
Abdullah Haktan <abdullahhaktan18@gmail.com>
2024-11-03 23:25:12 +0300

    the files added
    the files addet to the existing repository

---
+++ Models/EntityFramework/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ObsSistem.Models.EntityFramework
+{
+    public partial class myViewModel
+    {
+        public List<AkademisyenModel> akademisyenList { get; set; }
+        public List<bolumModel> bolumList { get; set; }
+    }
+
+    public partial class bolumListModel
+    {
+        public List<bolumModel> bolumList { get; set; }
+    }
+}
